71a9c201136af25b62fd8f90f24056c8b9e9a641
Wambaugh <marwambaugh@gmail.com>
2023-04-27 06:10:40 -0700

    debugJSreformatsteps

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,16 +3,16 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-window.deferredPrompt = event;
-console.log("it's working!");
+  window.deferredPrompt = event;
+  console.log("it's working!");
 
-butInstall.classList.toggle("hidden", false);
+  butInstall.classList.toggle("hidden", false);
 });
 
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+  const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
     return;
   }
@@ -25,5 +25,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+  window.deferredPrompt = null;
 });
